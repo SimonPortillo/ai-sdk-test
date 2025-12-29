b325d48d3620128ddfa1c48ebdda02d215ff8ca1
Simon Portillo <Simon.portillo@outlook.com>
2025-12-29 16:42:01 +0100

    refine system prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -17,13 +17,12 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: "openai/gpt-5-mini",
-    system: `You are a helpful assistant. You can only answer questions about projects in your knowledge base. Do not make up information about projects that are not in the knowledge base or make up follow up questions that you cannot answer using the tools provided. Do not ask the user if it wants to do an action you cannot perform like updating or deleting. If you are unsure about a question, respond with "I dunno".`,
+    system: `You are a helpful assistant. You can only answer questions about projects in your knowledge base. Do not make up information about projects that are not in the knowledge base or make up follow up questions/suggestions that you cannot answer using the tools provided. Do not ask the user if it wants to do an action you cannot perform like updating or deleting. If you are unsure about a question, respond with "I dunno".`,
     messages: await convertToModelMessages(messages),
     stopWhen: stepCountIs(5),
     tools: {
       addProject: tool({
-        description: `Add a project to your knowledge base.
-          If the user provides information about a project unprompted, use this tool without asking for confirmation.`,
+        description: `Add a project to your knowledge base.`,
         inputSchema: z.object({
           description: z
             .string()
@@ -40,7 +39,7 @@ export async function POST(req: Request) {
           createProject({ project_desc: description, project_title: title }),
       }),
       getInformation: tool({
-        description: `get project information from your knowledge base to answer questions. summarize the relevant information you find to best answer the user's question.`,
+        description: `get project information from your knowledge base to answer questions. Summarize the relevant information you find to best answer the user's question.`,
         inputSchema: z.object({
           question: z.string().describe("the users question"),
         }),
